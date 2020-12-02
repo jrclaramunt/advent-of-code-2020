ecc3c375e04e6c3f82dfe4b4a3bf4a7beee44c7c
J. Ramon Claramunt <jr.claramunt@gmail.com>
2020-12-02 20:48:58 +0100

    Fixed factory and added Day2

--- day_factory.py
+++ day_factory.py
@@ -1,11 +1,13 @@
 from day1.day1 import Day1
+from day2.day2 import Day2
 
 
 class DayFactory:
 
     def __init__(self, day, input_data):
         self.map = {
-            1: Day1(input_data)
+            1: Day1,
+            2: Day2
         }
         print(f'Day {day}\n')
-        print(self.map.get(day).solution())
+        print(self.map.get(day)(input_data).solution())
