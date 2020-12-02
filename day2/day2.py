e05d82512494461b86aef982435001dec565effc
J. Ramon Claramunt <jr.claramunt@gmail.com>
2020-12-02 20:49:38 +0100

    Added day 2 solution

---
+++ day2/day2.py
@@ -0,0 +1,40 @@
+import re
+
+
+class Day2:
+
+    def __init__(self, password_file):
+        self.password_file = password_file
+        self.regex = r'^(\d+)-(\d+) ([a-z]): ([a-z]*)$'
+
+    def solution(self):
+        print(f'Part 1: {self.part1()}')
+        print(f'Part 2: {self.part2()}')
+
+    def part1(self):
+        valid_passwords = 0
+
+        for entry in self.password_file:
+            tmp = re.search(self.regex, entry).groups()
+            min = int(tmp[0])
+            max = int(tmp[1])
+            character = tmp[2]
+            password = tmp[3]
+            if min <= password.count(character) <= max:
+                valid_passwords += 1
+
+        return valid_passwords
+
+    def part2(self):
+        valid_passwords = 0
+
+        for entry in self.password_file:
+            tmp = re.search(self.regex, entry).groups()
+            first_index = int(tmp[0]) - 1
+            second_index = int(tmp[1]) - 1
+            character = tmp[2]
+            password = tmp[3]
+            if (password[first_index] == character) ^ (password[second_index] == character):
+                valid_passwords += 1
+
+        return valid_passwords
