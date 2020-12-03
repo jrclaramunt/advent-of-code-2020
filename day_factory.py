8f6bd8490c693afb27d6e1f78b715a4e25ffaa9d
J. Ramon Claramunt <jr.claramunt@gmail.com>
2020-12-03 19:47:17 +0100

    Added Day 3 solution

--- day_factory.py
+++ day_factory.py
@@ -1,5 +1,6 @@
 from day1.day1 import Day1
 from day2.day2 import Day2
+from day3.day3 import Day3
 
 
 class DayFactory:
@@ -7,7 +8,8 @@ class DayFactory:
     def __init__(self, day, input_data):
         self.map = {
             1: Day1,
-            2: Day2
+            2: Day2,
+            3: Day3
         }
         print(f'Day {day}\n')
         print(self.map.get(day)(input_data).solution())
